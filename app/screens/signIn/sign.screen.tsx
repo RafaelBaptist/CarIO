d24afbb716464fb2d81c0b916638a0ede790cef8
Rafael de Souza Baptista <faelbaptista0@gmail.com>
2025-05-21 16:20:58 -0300

    signScreen-Logo

--- app/screens/signIn/sign.screen.tsx
+++ app/screens/signIn/sign.screen.tsx
@@ -1,10 +1,21 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
-
+import {createStyles} from './signin.styles';
+import assets from '../../assets';
 const SignInScreen = () => {
+  const styles = createStyles();
+  const {logoBlack} = assets;
   return (
-    <View>
-      <Text>SignInScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.flexRow}>
+        <Image source={logoBlack} style={styles.carLogo} />
+        <Text style={styles.textLogo}>CarIO</Text>
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>Wellcome Back</Text>
+        <Text style={styles.textStyle}>Ready to hit the road.</Text>
+      </View>
     </View>
   );
 };
