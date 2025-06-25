6ce75627f1824f937f6f027a18cade4689289077
Rafael de Souza Baptista <faelbaptista0@gmail.com>
2025-06-25 16:39:59 -0300

    fixed-home-navigator

--- app/screens/home/home.screen.tsx
+++ app/screens/home/home.screen.tsx
@@ -1,11 +1,20 @@
-import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
+import {View, Text, SafeAreaView, Image} from 'react-native';
+import {createStyles} from './home.styles';
+import assets from '../../assets';
 
 const HomeScreen = () => {
+  const {logoBlack} = assets;
+  const styles = createStyles();
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.flexRow}>
+          <Image source={logoBlack} style={styles.carLogo} />
+          <Text style={styles.titleStyle}>CarIO</Text>
+        </View>
+      </View>
+    </View>
   );
 };
 
--- app/screens/home/home.styles.ts
+++ app/screens/home/home.styles.ts
@@ -1,12 +1,36 @@
 import {StyleSheet} from 'react-native'
 import { colors } from '../../theme/colors';
-import {scale} from '../../theme/scale'
+import { scale } from '../../theme/scale';
 import { FontSize } from '../../theme/font-size';
+import { typography } from '../../theme/typography';
+
 export const createStyles= ()=>StyleSheet.create({
 container:{
     flex:1,
-    backgroundColor:colors.white,
+    backgroundColor: colors.background,
+
 },
 
-
+carLogo: {
+    height: scale(45),
+    width: scale(45),
+    borderRadius: 100,
+  },   
+   flexRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: scale(12),
+    paddingVertical: scale(12),
+  },
+  titleStyle: {
+    fontSize: FontSize.FONT_24Px,
+    color: colors.black,
+    flex: 1,
+    fontFamily: typography.bold,
+  },
+header:{
+    borderBottomWidth:1,
+    borderBottomColor:colors.border,
+    paddingHorizontal:scale(18),
+},
 })
